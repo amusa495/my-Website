444478503ffed1294ed341f605ac38587a8f30fc
amusa495 <lateefamusa689@gmail.com>
2025-12-31 13:41:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function showGoal() {
+  const goalText = document.getElementById("goal-text");
+
+  goalText.textContent =
+    `My goal is to grow academically and personally by building a strong educational foundation, developing good habits, and preparing myself for future opportunities. I aim to stay focused, disciplined, and consistent in my studies while continually striving to improve and achieve my full potential.`;
+}
